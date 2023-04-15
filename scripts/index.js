1e9736b06787e0d465d6f6f5c6cf5adc5f65efb4
Egor <golderandlomove@gmail.com>
2023-04-15 19:39:37 +0500

    По человечески сделал форму через правильные теги и кнопки

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,47 @@
 'use strict';
 
-navigator.geolocation.getCurrentPosition(
-  (position) => {
-    console.log(position);
-  },
-  () => {
-    console.log('Ну ты и пипец');
-  }
-);
+const workoutCreateSide = document.querySelector('.workout-create');
 
+// navigator.geolocation.getCurrentPosition(
+//   (position) => {
+//     console.log(position);
+//   },
+//   () => {
+//     console.log('Ну ты и пипец');
+//   }
+// );
+
+let map;
+
+// По готовности размещаем карту на страницу и зсоздаем прослушку на клик по ней
 ymaps.ready(() => {
-  let map = new ymaps.Map('map', {
+  map = new ymaps.Map('map', {
     center: [55.16009176823716, 61.40247375140378],
     zoom: 15,
   });
 
-  console.log('YMaps Ready!');
+  map.events.add('click', (e) => {
+    const coords = e.get('coords');
+
+    // Создаем новый гео-объект
+    const newGeoObj = new ymaps.GeoObject(
+      {
+        geometry: {
+          type: 'Point',
+          coordinates: coords,
+        },
+        properties: {
+          iconContent: 'Тестовая метка',
+        },
+      },
+      {
+        preset: 'islands#blackStretchyIcon',
+        draggable: true,
+      }
+    );
+    // Помещаем ГО на карту
+    map.geoObjects.add(newGeoObj);
+
+    workoutCreateSide.classList.remove('hidden');
+  });
 });
